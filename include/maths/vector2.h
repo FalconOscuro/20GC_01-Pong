e502e3204960330dbb813fb27a295d0daa7272d7
Peter Garrod <p.jlgarrod@gmail.com>
2023-09-27 12:26:04 +0100

    Vector operators

--- include/maths/vector2.h
+++ include/maths/vector2.h
@@ -7,6 +7,18 @@ public:
     Vector2();
     Vector2(float x, float y);
 
+    Vector2 operator+(const Vector2& v) const;
+    Vector2 operator-(const Vector2& v) const;
+
+    Vector2 operator*(const float& s) const;
+    Vector2 operator/(const float& s) const;
+
+    void operator+=(const Vector2& v);
+    void operator-=(const Vector2& v);
+
+    Vector2 operator*=(const float& s);
+    Vector2 operator/=(const float& s);
+
     float X;
     float Y;
 };
--- src/maths/vector2.cpp
+++ src/maths/vector2.cpp
@@ -6,3 +6,47 @@ Vector2::Vector2()
 Vector2::Vector2(float x, float y):
     X(x), Y(y)
 {}
+
+Vector2 Vector2::operator+(const Vector2& v) const
+{
+    return Vector2(X + v.X, Y + v.Y);
+}
+
+Vector2 Vector2::operator-(const Vector2& v) const
+{
+    return Vector2(X - v.X, Y - v.Y);
+}
+
+Vector2 Vector2::operator*(const float& s) const
+{
+    return Vector2(X * s, Y * s);
+}
+
+Vector2 Vector2::operator/(const float& s) const
+{
+    return Vector2(X / s, Y / s);
+}
+
+void Vector2::operator+=(const Vector2& v)
+{
+    X += v.X;
+    Y += v.Y;
+}
+
+void Vector2::operator-=(const Vector2& v)
+{
+    X -= v.X;
+    Y -= v.Y;
+}
+
+void Vector2::operator*=(const float& s)
+{
+    X *= s;
+    Y *= s;
+}
+
+void Vector2::operator/=(const float& s)
+{
+    X /= s;
+    Y /= s;
+}
